d9f290b85d6dd12868e78f821fa03e4e37d26e09
Samantha <samantha.c.alvarenga@gmail.com>
2024-11-25 22:23:41 -0300

    adicionado formulario e style. E fazer correções no style

---
+++ cadastro.php
@@ -0,0 +1,18 @@
+<div class="form-container">
+
+    <h2>Cadastro</h2>
+
+    <form action="cadastro.php" method="post">
+
+        <label for="nome">Nome:</label>
+        <input type="text" id="nome" name="nome" required>
+        <label for="email">Email Educacional:</label>
+        <input type="email" id="email" name="email" required>
+        <label for="senha">Senha:</label>
+        <input type="password" id="senha" name="senha" required>
+
+        <button type="submit">Cadastrar</button>
+
+    </form>
+
+</div>
