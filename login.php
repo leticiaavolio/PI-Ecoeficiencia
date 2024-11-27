1cd0e91607fd7807417b19acd9b3c7bacc5bedc3
Samantha <samantha.c.alvarenga@gmail.com>
2024-11-26 22:29:08 -0300

    tela do login em andamento

---
+++ login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styleLogin.css">
+    <link rel="stylesheet" href="forms.css">
+    <title>Login Colaboradores</title>
+</head>
+
+<body>
+
+    <div class="container">
+
+        <form method="post">
+            Email: <input type="email" name="email" required><br><br>
+            Senha: <input type="password" name="senha" required><br><br>
+            <input type="submit" valule="Entrar">
+        </form>
+
+    </div>
+
+    </div>
+</body>
+
+</html>
