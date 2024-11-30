b0cca0b5fe0e71da32dd71d0c803cea2404469c4
Samantha Alvarenga <samantha.c.alvarenga@gmail.com>
2024-11-29 22:50:02 -0300

    conexão do login com o banco. fazendo verificação de login de cada session e bloquando-o

--- login.php
+++ login.php
@@ -1,3 +1,71 @@
+<?php
+
+include 'conexao.php';
+
+session_start();
+
+
+// Verifica se o formulário foi enviado
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Pega o email e a senha do formulário
+    $login = strtoupper($_POST['login']);
+    $senha = $_POST['senha'];
+
+   
+    // Proteção contra injeção SQL
+    $email = $conn->real_escape_string($login);
+    $senha = $conn->real_escape_string($senha);
+
+    // Query para verificar o email
+    $sql = "SELECT * FROM usuarios WHERE email = '$login'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // Usuário encontrado
+        $usuario = $result->fetch_assoc();
+
+        // Verifica se a senha fornecida corresponde ao hash no banco de dados
+        if (password_verify($senha, $usuario['senha'])) {
+            // A senha está correta, redireciona conforme o perfil
+            switch ($usuario['perfil']) {
+                case 'colaborador':
+                    $_SESSION['usuario_id'] = ($usuario['idUsuario']);
+                    $_SESSION['perfil'] = ($usuario['perfil']);
+                    $_SESSION['login'] = ($usuario['email']);
+                    header("Location: index.php?page=doacao"); 
+                    exit();
+                    
+                case 'adm':
+                    $_SESSION['usuario_id'] = ($usuario['idUsuario']);
+                    $_SESSION['perfil'] = ($usuario['perfil']);
+                    $_SESSION['login'] = ($usuario['email']);
+                    header("Location: index.php?page=relatorio"); 
+                    exit();
+                case 'doador':
+                    
+                    $_SESSION['usuario_id'] = ($usuario['idUsuario']);
+                    $_SESSION['perfil'] = ($usuario['perfil']);
+                    $_SESSION['login'] = ($usuario['email']);
+                    header("Location: index.php?page=verSaldo");                     
+                    exit();
+                default:
+                    echo "Perfil de usuário desconhecido.";
+                    exit();                    
+            }
+
+           
+        } else {
+            echo "E-mail ou senha inválidos.";
+        }
+    } else {
+        echo "E-mail ou senha inválidos.";
+    }
+}
+
+// Fecha a conexão com o banco de dados
+$conn->close();
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -12,14 +80,14 @@
 
 <body>
 
-    <div class="login-form">
+    <div class="form-container">
         <h2>Login</h2>
 
         <form method="post">
-            <label for="emailLogin"></label>
-            <input type="email" placeholder="Email" id="emailLogin" name="email" required>
-            <label for="senhaLogin"></label>
-            <input type="password" placeholder="Senha" id="senhaLogin" name="senha" required>
+            <label for="login"></label>
+            <input placeholder="Email" id="login" name="login" required>
+            <label for="senha"></label>
+            <input type="password" placeholder="Senha" id="senha" name="senha" required>
             <button type="submit">Entrar</button>
         </form>
 
@@ -28,5 +96,5 @@
     </div>
 </body>
 
-</html>
 
+</html>
