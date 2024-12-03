92d699998054389dc9da4ec885911fea5cb877c2
Samantha Alvarenga <samantha.c.alvarenga@gmail.com>
2024-12-02 21:25:19 -0300

    juntando arquivo do cadastro.php. verificaçãp de usuario está logado e verificando qual o perfil

--- cadastro.php
+++ cadastro.php
@@ -2,6 +2,27 @@
 session_start();
 include 'conexao.php';
 
+
+    session_start();
+    // Inicia a verificação da sessão para garantir que o usuário esteja logado e tenha um perfil válido.
+    if(!isset($_SESSION['usuario_id']) || !isset($_SESSION['perfil'])) {
+        // Se a variável de sessão 'usuario_id' ou 'perfil' não estiverem definidas, o usuário não está logado ou não tem perfil
+        // Redireciona o usuário para a página de login
+        header("Location: index.php?page=login ");
+        exit;
+    }
+            
+        
+    // Inicia a verificação do perfil do usuário para garantir que ele tenha um perfil autorizado
+    if ($_SESSION['perfil'] != "colaborador" && $_SESSION['perfil'] != "adm"){
+        
+        // Se o perfil do usuário NÃO for "colaborador" e NEM "adm", significa que ele tem outro perfil (por exemplo, "doador")
+        // Redireciona o usuário para a página onde ele pode visualizar o saldo, já que ele não tem acesso à página atual
+        header("Location: index.php?page=verSaldo ");
+        // Após o redirecionamento, a execução do código é interrompida para garantir que nada mais seja processado
+        exit;
+    }
+    
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
     // Captura os dados do formulário
     $nome = $_POST['nome'];
@@ -65,7 +86,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
 
         <form action="cadastro.php" method="post">
-            <select name = "perfil">
+            <select name="perfil">
                 <option selected disabled value="">Selecionar Usuário</option>
                 <option value="colaborador">Colaboradores</option>
                 <option value="adm">Adm</option>
@@ -74,9 +95,9 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
             <label for="nome">Nome:</label>
             <input type="text" id="nome" name="nome" required>
             <label for="login">Login:</label>
-            <input placeholder= "Nome mais iniciais do Sobrenome Ex Paulo.sca" id="login" name="login" required>
+            <input placeholder="Nome mais iniciais do Sobrenome Ex Paulo.sca" id="login" name="login" required>
             <label for="senha">Senha:</label>
-            <input  placeholder= "Senha númerica, apenas 4 dígitos"  type="password" id="senha" name="senha" required>
+            <input placeholder="Senha númerica, apenas 4 dígitos" type="password" id="senha" name="senha" required>
             <button type="submit">Cadastrar</button>
 
         </form>
